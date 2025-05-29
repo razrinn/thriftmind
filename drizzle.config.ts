aeed3ebded5f140fe475da76ab445d6573f88a08
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-29 16:09:51 +0700

    init drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+
+// Only run from local/pipeline
+export default defineConfig({
+	out: './src/db/migrations',
+	schema: './src/db/**.schema.ts',
+	dialect: 'sqlite',
+	driver: 'd1-http',
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		token: process.env.CLOUDFLARE_D1_TOKEN!,
+	},
+});
