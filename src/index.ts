08c6181ca041f5b6df127aba6f033dc7e3aafd48
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-29 12:00:33 +0700

    disable cron

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ export default {
 	},
 
 
-	async scheduled(event, env, ctx): Promise<void> {
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+	// async scheduled(event, env, ctx): Promise<void> {
+	// 	let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
+	// 	let wasSuccessful = resp.ok ? 'success' : 'fail';
 
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
-	},
+	// 	console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+	// },
 } satisfies ExportedHandler<Env>;
