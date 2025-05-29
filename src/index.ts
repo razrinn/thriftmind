7e0c0064795806a9f60986d406d0efb2c9f051af
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-29 13:16:21 +0700

    use hono

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-import { Bot, webhookCallback } from "grammy";
+import { Bot, webhookCallback } from 'grammy';
+import { Hono } from 'hono';
 
+const app = new Hono<{ Bindings: Env }>();
+
+app
+	.get('/', (c) => {
+		return c.redirect('https://t.me/thriftmind_bot');
+	})
+	.use('/webhook', async (c, next) => {
+		const rawBody = await c.req.text();
+
+		if (!rawBody) {
+			return c.text('Empty request body', 400);
+		}
+
+		try {
+			c.req.parseBody = JSON.parse(rawBody); // Optional if JSON parsing is needed
+		} catch (error) {
+			return c.text('Invalid JSON body', 400);
+		}
+
+		await next();
+	})
+	.post('/webhook', (c) => {
+		const bot = new Bot(c.env.BOT_TOKEN, { botInfo: c.env.BOT_INFO });
+
+		bot.command('start', async (ctx) => {
+			await ctx.reply('Hello, world!');
+		});
+
+		return webhookCallback(bot, 'hono')(c);
+	});
 
 export default {
-	async fetch(req, env, ctx) {
-    const bot = new Bot(env.BOT_TOKEN, { botInfo: env.BOT_INFO});
-
-
-    bot.command("start", async (c) => {
-      await c.reply("Hello, world!");
-    });
-
-    return webhookCallback(bot, "cloudflare-mod")(req);
-	},
-
+	fetch: app.fetch,
 
 	// async scheduled(event, env, ctx): Promise<void> {
 	// 	let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
